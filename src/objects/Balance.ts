21b4cca3ef1cb0ad89e53c97c2e3e152802d1775
Luísa Rodrigues Foppa <lulufoppa@gmail.com>
2024-02-28 17:03:25 -0300

    feat: creating balance class

---
+++ src/objects/Balance.ts
@@ -0,0 +1,10 @@
+
+export default class Balance{
+    value: number;
+
+    constructor( value: number){
+        this.value = value;
+        // this.balanceText = this.scene.add.text(10, 10, 'Balance: $0', { fontSize: '32px', fill: '#fff' });
+    }
+
+}
--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,11 +1,14 @@
 import PhaserImg from '@objects/PhaserImg';
+import Balance from '@objects/Balance';
 
 export class GameScene extends Phaser.Scene {
   constructor() {
     super({ key: 'GameScene' });
   }
+  
+  balance = new Balance(100);
 
-  init(): void {}
+  init(): void { console.log(this.balance)}
 
   create(): void {
     const phaser = PhaserImg.create(this, 400, 100);
