0431d4368a77f9c9dfae506fc8037e7bd5178789
Luísa Rodrigues Foppa <lulufoppa@gmail.com>
2024-02-29 11:15:59 -0300

    feat: balance text & create Rectangles function

--- src/objects/Reel.ts
+++ src/objects/Reel.ts
@@ -5,38 +5,39 @@ export default class Reel extends Phaser.GameObjects.Container {
 
     fruits = new Fruits().getFruits();
     numberOfFruits = this.fruits.length;
+    tweens: any;
 
     constructor(scene: Phaser.Scene, x: number, y: number, rectangle : Phaser.GameObjects.Rectangle) {
         super(scene, x, y);
         this.reel = this.scene.add.group();
         this.reel.create(x, y, this.fruits[0].fruit);
         this.reel.create(x, y + 100, this.fruits[1].fruit);
+        this.reel.create(x, y + 200, this.fruits[2].fruit);
         this.reel.create(x, y - 100, this.fruits[this.numberOfFruits - 1].fruit);
-        // this.fruits.forEach((fruit: string, index: number) => {
-        //     this.reel.create(x, y + index* 100 , fruit);
-        // }
-        // );
-        // this.reel.create(x, y + 100, 'banana');
-        // this.reel.create(x, y - 100, 'banana');
+        this.reel.create(x, y - 200, this.fruits[this.numberOfFruits - 2].fruit);
+
         this.rectangle = rectangle;
     }
 
-        // startSpin() {
-        // // Lógica para girar os reels
-        // for (let i = 0; i < numberOfReels; i++) {
-        //     const reel = this.reels[i];
-        //     const spinTween = this.tweens.add({
-        //         targets: reel.map(symbol => symbol.y),
-        //         y: reel[0].y + spinDistance,
-        //         ease: 'Cubic.easeInOut',
-        //         duration: spinDuration,
-        //         delay: i * 200,
-        //         onComplete: () => {
-        //             // Lógica a ser executada quando a rotação do reel for concluída
-        //             this.stopReel(reel);
-        //         }
-        //     });
-        // }
+    // startSpin() {
+    //     // Lógica para girar os reels
+    //         const spinTween = this.tweens.add({
+    //             targets: this.reel.getChildren(),
+    //             y: this.reel.y + spinDistance,
+    //             ease: 'Cubic.easeInOut',
+    //             duration: spinDuration,
+    //             delay: 200,
+    //             onComplete: () => {
+    //                 // Lógica a ser executada quando a rotação do reel for concluída
+    //                 this.stopReel(this.reel);
+    //             }
+    //         });
+    // }
+
+    // stopReel(reel: Phaser.GameObjects.Sprite[]) {
+    //     // Lógica para parar o reel
+    //         // const symbolIndex = this.selectSymbolIndex();
+    //         // reel[i].setTexture(this.symbols[symbolIndex]);
     // }
 
 }
--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -2,6 +2,11 @@ import Balance from '@objects/Balance';
 import Reel from '@objects/Reel';
 
 export class GameScene extends Phaser.Scene {
+
+  reel1: Reel;
+  reel2: Reel;
+  reel3: Reel;
+
   constructor() {
     super({ key: 'GameScene' });
   }
@@ -9,27 +14,30 @@ export class GameScene extends Phaser.Scene {
   balance = new Balance(100);
 
 
-  init(): void { console.log(this.balance)
-  console.log(this.balance.getValue());}
+  init(): void {}
 
   create(): void {
-    this.createRectangles();
+    const cameraWidth= this.cameras.main.width;
+    const cameraHeight= this.cameras.main.height;
+    this.createRectangles(cameraWidth, cameraHeight);
+
+    const balanceText = this.add.text( cameraWidth < 600? cameraWidth/4: cameraWidth/1.3, cameraWidth < 600? 50: cameraHeight/4, `Saldo: ${this.balance.getValue()}`, { fontSize: '24px', color: '#ffffff' });
   }
 
   preload(): void {}
 
   update(): void {}
 
-  public createRectangles() {
-    const slotRect1 = this.add.rectangle(this.cameras.main.width / 4, this.cameras.main.height / 2, this.cameras.main.width * 0.15, 350);
+  // Cria os retângulos que representam os rolos da máquina
+  public createRectangles(camWidth: number, camHeight: number) {
+    const slotRect1 = this.add.rectangle(camWidth < 600? camWidth/3: camWidth/4, camHeight/2, camWidth * 0.15, 350);
     slotRect1.setStrokeStyle(2, 0xcedce6);
-    const reel1 = new Reel(this, slotRect1.x, slotRect1.y, slotRect1);
-    const slotRect2 = this.add.rectangle(slotRect1.x + slotRect1.width, this.cameras.main.height / 2, this.cameras.main.width * 0.15, 350);
+    this.reel1 = new Reel(this, slotRect1.x, slotRect1.y, slotRect1);
+    const slotRect2 = this.add.rectangle(slotRect1.x + slotRect1.width, camHeight / 2, camWidth * 0.15, 350);
     slotRect2.setStrokeStyle(2, 0xcedce6);
-    const reel2 = new Reel(this, slotRect2.x, slotRect2.y, slotRect2);
-    const slotRect3 = this.add.rectangle(slotRect2.x + slotRect2.width, this.cameras.main.height / 2, this.cameras.main.width * 0.15, 350);
+    this.reel2 = new Reel(this, slotRect2.x, slotRect2.y, slotRect2);
+    const slotRect3 = this.add.rectangle(slotRect2.x + slotRect2.width, camHeight / 2, camWidth * 0.15, 350);
     slotRect3.setStrokeStyle(2, 0xc3dce6);
-    const reel3 = new Reel(this, slotRect3.x, slotRect3.y, slotRect3);
+    this.reel3 = new Reel(this, slotRect3.x, slotRect3.y, slotRect3);
   }
-
 }
