f93a7b1bc89c0c5d2a641e7cd620569028ae1380
Luísa Rodrigues Foppa <lulufoppa@gmail.com>
2024-02-28 23:52:37 -0300

    feat: more details to fruits, fruit interface

--- src/objects/Fruits.ts
+++ src/objects/Fruits.ts
@@ -1,14 +1,21 @@
+interface Fruit {
+    fruit: string;
+    weight: number;
+    payment: number;
+}
+
 export default class Fruits {
-    fruits: string[];
+    fruits: Fruit[];
     constructor() {
         this.fruits = [
-            'banana',
-            'apple',
-            'cherry',
-            'orange',
-            'lemon',
-            'grape',
-            'watermelon'
+            {fruit : 'banana', weight : 8, payment: 10},
+            {fruit : 'apple', weight : 6, payment: 15},
+            {fruit : 'cherry', weight : 4, payment: 50},
+            {fruit : 'orange', weight : 8, payment: 10},
+            {fruit : 'lemon', weight : 6, payment: 25},
+            {fruit : 'grape', weight : 8, payment: 10},
+            {fruit : 'watermelon', weight : 6, payment: 20},
+            {fruit : 'slot', weight : 1, payment: 100}
         ];
     }
     getFruits() {
--- src/objects/Reel.ts
+++ src/objects/Reel.ts
@@ -9,9 +9,9 @@ export default class Reel extends Phaser.GameObjects.Container {
     constructor(scene: Phaser.Scene, x: number, y: number, rectangle : Phaser.GameObjects.Rectangle) {
         super(scene, x, y);
         this.reel = this.scene.add.group();
-        this.reel.create(x, y, this.fruits[0]);
-        this.reel.create(x, y + 100, this.fruits[1]);
-        this.reel.create(x, y - 100, this.fruits[this.numberOfFruits - 1]);
+        this.reel.create(x, y, this.fruits[0].fruit);
+        this.reel.create(x, y + 100, this.fruits[1].fruit);
+        this.reel.create(x, y - 100, this.fruits[this.numberOfFruits - 1].fruit);
         // this.fruits.forEach((fruit: string, index: number) => {
         //     this.reel.create(x, y + index* 100 , fruit);
         // }
@@ -20,4 +20,23 @@ export default class Reel extends Phaser.GameObjects.Container {
         // this.reel.create(x, y - 100, 'banana');
         this.rectangle = rectangle;
     }
+
+        // startSpin() {
+        // // Lógica para girar os reels
+        // for (let i = 0; i < numberOfReels; i++) {
+        //     const reel = this.reels[i];
+        //     const spinTween = this.tweens.add({
+        //         targets: reel.map(symbol => symbol.y),
+        //         y: reel[0].y + spinDistance,
+        //         ease: 'Cubic.easeInOut',
+        //         duration: spinDuration,
+        //         delay: i * 200,
+        //         onComplete: () => {
+        //             // Lógica a ser executada quando a rotação do reel for concluída
+        //             this.stopReel(reel);
+        //         }
+        //     });
+        // }
+    // }
+
 }
