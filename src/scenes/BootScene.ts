226a1372854c5cc6ad2b4ed5175793137a56bb5e
Luísa Rodrigues Foppa <lulufoppa@gmail.com>
2024-03-22 13:42:17 -0300

    feat: updating fruit assets

--- src/scenes/BootScene.ts
+++ src/scenes/BootScene.ts
@@ -1,11 +1,11 @@
-import bananaImg from '@assets/fruits/Banana_96x96.png';
-import appleImg from '@assets/fruits/Apple_96x96.png';
-import cherryImg from '@assets/fruits/Cherry_96x96.png';
-import orangeImg from '@assets/fruits/Orange_96x96.png';
-import lemonImg from '@assets/fruits/Lemon_96x96.png';
-import grapeImg from '@assets/fruits/Red_Grape_96x96.png';
-import watermelonImg from '@assets/fruits/Watermelon_96x96.png';
-import slotImg from '@assets/fruits/slot.png';
+import bananaImg from '@assets/fruits/banana.png';
+import appleImg from '@assets/fruits/apple.png';
+import cherryImg from '@assets/fruits/cherry.png';
+import orangeImg from '@assets/fruits/orange.png';
+import lemonImg from '@assets/fruits/lemon.png';
+import grapeImg from '@assets/fruits/grape.png';
+import watermelonImg from '@assets/fruits/watermelon.png';
+import barImg from '@assets/fruits/bar.png';
 import yellowButtonM from '@assets/ui/CGB02-yellow_M_btn.png';
 import yellowButtonS from '@assets/ui/CGB02-yellow_S_btn.png';
 export class BootScene extends Phaser.Scene {
@@ -23,7 +23,7 @@ export class BootScene extends Phaser.Scene {
     this.load.image('lemon', lemonImg);
     this.load.image('grape', grapeImg)
     this.load.image('watermelon', watermelonImg);
-    this.load.image('slot', slotImg);
+    this.load.image('bar', barImg);
   }
 
   update(): void {
